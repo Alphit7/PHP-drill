f5d7a8b9c13bf686fa62ef05df6ceec93cc90d4b
Alphit7 <jacobs.sylvain@gmail.com>
2023-06-05 10:24:19 +0200

    first excuse done

--- excuses.php
+++ excuses.php
@@ -10,21 +10,21 @@
         <input type="text" id="name" name="name">
         <fieldset>
             <legend>Select the Gender of your child</legend>
-            <input type="radio" name="gender" id="Boy" value="Boy" checked>
+            <input type="radio" name="gender" id="Boy" value="boy" checked>
             <label for="Boy">Boy</label>
-            <input type="radio" name="gender" id="Girl" value="Girl">
+            <input type="radio" name="gender" id="Girl" value="girl">
             <label for=" Girl">Girl</label>
         </fieldset>
         <label for="teacher">Name of the Teacher</label>
         <input type="text" id="teacher" name="teacher">
         <fieldset>
             <legend>Reason for the absence</legend>
-            <input type="radio" name="absence" id="illness" value="illness" checked>
-            <label for="illness">Illness</label>
-            <input type="radio" name="absence" id="death" value="death">
-            <label for="death">Death of a family member</label>
-            <input type="radio" name="absence" id="activity" value="activity">
-            <label for="activity">Significant extra-curricular activity</label>
+            <input type="radio" name="absence" id="illness" value="doctor's apointment" checked>
+            <label for="illness">illness</label>
+            <input type="radio" name="absence" id="death" value="funeral">
+            <label for="death">death of a family member</label>
+            <input type="radio" name="absence" id="activity" value="important extra-curricular event">
+            <label for="activity">significant extra-curricular activity</label>
             <input type="radio" name="absence" id="other" value="">
             <input type="text" name="absence_other" id="other__input">
             <label for="other">Other</label>
@@ -32,12 +32,18 @@
         <button type="submit">Submit</button>
     </form>
     <?php
-    if (isset($_GET["name"]) and isset($_GET["gender"]) and isset($_GET["teacher"])) {
+    if (isset($_GET["name"]) and isset($_GET["gender"]) and isset($_GET["teacher"]) and isset($_GET["absence"]) and $_GET["absence_other"] == "") {
         $name = $_GET["name"];
         $gender = $_GET["gender"];
         $teacher = $_GET["teacher"];
+        $absence = $_GET["absence"];
+    } else {
+        $name = $_GET["name"];
+        $gender = $_GET["gender"];
+        $teacher = $_GET["teacher"];
+        $absence = $_GET["absence_other"];
     }
-
+    echo "Dear $teacher, my $gender won't be able to come to school tommorow and I am really sorry about it. $name has to attend a $absence. I hope you understand, have a good day.";
     ?>
 </body>
 
